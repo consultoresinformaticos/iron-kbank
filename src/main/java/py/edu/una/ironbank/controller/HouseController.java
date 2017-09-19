e46b18026b32fb2062d583a4a978cb7c5425e989
Enrique Rodriguez <erodriguez@consultoresinformaticos.com.py>
2017-09-18 22:57:52 -0400

    Se agregan los servios en HouseController

--- src/main/java/py/edu/una/ironbank/controller/HouseController.java
+++ src/main/java/py/edu/una/ironbank/controller/HouseController.java
@@ -1,13 +1,15 @@
 package py.edu.una.ironbank.controller;
 
-
-
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,9 @@ import py.edu.una.ironbank.repository.HouseRepository;
 public class HouseController {
 
 	public static final String GET_HOUSES = "/house";
+	public static final String ADD_HOUSE = "/house/add";
+	public static final String DELETE_HOUSE = "/house/delete/{id}";
+	public static final String UPDATE_HOUSE = "/house/update";
 
 	@Autowired
 	private HouseRepository houseRepository;
@@ -33,5 +38,46 @@ public class HouseController {
 		}
 		return new ResponseEntity<List<House>>(houseList, HttpStatus.OK);
 	}
+	
+	
+	@RequestMapping(value = ADD_HOUSE, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+	public ResponseEntity<House> addHouse(@Valid @RequestBody(required = true) House house) {
+		house = houseRepository.save(house);
+		return new ResponseEntity<House>(house, HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(value = UPDATE_HOUSE, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+	public ResponseEntity<House> updateHouse(@RequestBody(required = true) House house) {
+		System.out.println(""+ house);
+
+		if(houseRepository.exists(house.getId())){
+			house = houseRepository.save(house);
+			return new ResponseEntity<House>(house, HttpStatus.OK);
+		} else{
+			return new ResponseEntity<House>(HttpStatus.NO_CONTENT);
+		}
+		
+		
+	}
+	
+	@RequestMapping(value = DELETE_HOUSE, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+	public ResponseEntity<House> deleteHouse(@PathVariable("id") int id) {
+		try {
+			House house = houseRepository.findOne(id);
+			if(house == null){
+				return new ResponseEntity<House>(HttpStatus.NOT_FOUND);
+			} else{
+				houseRepository.delete(house);
+				return new ResponseEntity<House>(HttpStatus.OK);
+			}
+			
+		} catch (Exception e) {
+			return new ResponseEntity<House>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
+	
 
 }
--- src/main/java/py/edu/una/ironbank/entity/House.java
+++ src/main/java/py/edu/una/ironbank/entity/House.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -17,9 +18,10 @@ public class House implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO, generator = "casa_id_seq")
+	 @SequenceGenerator(name = "casa_id_seq", sequenceName = "casa_id_seq")
+	  @Column(name = "ID")
 	private Integer id;
 	@Column(name = "nombre")
 	private String name;
